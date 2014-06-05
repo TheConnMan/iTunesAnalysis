86b06b72230bd65ea17ace2ff21c97c7f6c03d1e
TheConnMan <bcconn2112@gmail.com>
2014-06-04 22:30:37 -0400

    Added distribution graph type. Closes #9, closes #8, and closes #7. Abstracted a few functions.

--- js/iTunes.js
+++ js/iTunes.js
@@ -1,4 +1,4 @@
-var full, width, margin = {left: 20, right: 40, top: 50, bottom: 20},
+var full, width,
 	height = 600, pad = 5, delay = 1500,
 	fields = ['Name', 'Artist', 'Album', 'Genre', 'Total Time', 'Date Added', 'Play Count', 'Play Date UTC', 'Skip Count', 'Rating'];
 
@@ -64,54 +64,30 @@ function parseXML(xml) {
  * Creates all visualizations
  */
 function createAll() {
-	createTop('Play');
-	createTop('Skip');
+	createTop('Play Count', '#topPlay');
+	createTop('Skip Count', '#topSkip');
+	createDistribution('Play Count', '#playDistribution', 5);
+	createDistribution('Total Time', '#timeDistribution', 10, function(d) { return d / 1000; });
 }
 
-// Creates top chart
-function createTop(metric) {
+/**
+ * Creates a top graph.
+ * @param z - Metric
+ * @param id - Id of chart container
+ */
+function createTop(z, id) {
+	var margin = {left: 20, right: 40, top: 50, bottom: 20};
 	// Remove old svg
-	d3.select('#top' + metric).select('svg').remove();
-	// Adjust default parameters
-	margin.left = 10;
-	var z = metric + ' Count';
+	d3.select(id).select('svg').remove();
 	
 	// Create SVG
-	var svg = d3.select('#top' + metric).append('svg')
+	var svg = d3.select(id).append('svg')
 		.attr('width', width).attr('height', height);
 	
 	// Initialize genre data
-	var genres = [];
-	$.each(full.reduce(function(all, cur) {
-		if (all[cur['Genre']]) {
-			all[cur['Genre']]++;
-		} else if(cur['Genre']) {
-			all[cur['Genre']] = 1;
-		};
-		return all;
-	}, {}), function(k, v) {
-		genres.push({name: k, val: v});
-	});
-	
-	genres = createLegend(genres, svg, 'Top Genres', filter);
+	var genres = createLegend(full.slice(0), 'Genre', svg, 'Top Genres', refresh);
 	
 	refresh([])
-	
-	// Update filters
-	function filter(d, me) {
-		var classes = me.attr('class').split(' ');
-		if (classes.length == 1) {
-			me.select('rect').style('fill', d.color);
-			me.select('text').style('fill', 'white');
-			me.attr('class', classes[0] + ' selected');
-		} else {
-			me.select('rect').style('fill', 'none');
-			me.select('text').style('fill', '#555555');
-			me.attr('class', classes[0]);
-		}
-		var selected = svg.selectAll('.selected')[0].map(function(d) { return d3.select(d).select('text').text(); });
-		refresh(selected);
-	}
 	
 	// Refreshes data
 	function refresh(selected) {
@@ -122,44 +98,44 @@ function createTop(metric) {
 		
 		// Create bars and count text
 		var scale = d3.scale.linear().domain([0, d3.max(data, function(d) { return d.data[z]; })]).range([0, width - margin.right - x0]);
-		svg.selectAll('.topSong-name').data(data).exit().remove();
-		svg.selectAll('.topSong-data').data(data).exit().remove();
-		svg.selectAll('.topSong-count').data(data).exit().remove();
+		svg.selectAll('.top-name').data(data).exit().remove();
+		svg.selectAll('.top-data').data(data).exit().remove();
+		svg.selectAll('.top-count').data(data).exit().remove();
 		
-		svg.selectAll('.topSong-name, .topSong-count').transition().duration(delay).style('opacity', 0);
+		svg.selectAll('.top-name, .top-count').transition().duration(delay).style('opacity', 0);
 		
 		setTimeout(function() {
-			var nameText = svg.selectAll('.topSong-name').data(data).enter()
+			var nameText = svg.selectAll('.top-name').data(data).enter()
 				.append('text')
-				.attr('class', 'topSong-name')
+				.attr('class', 'top-name')
 				.attr('transform', function(d) { return 'translate(' + (x0 - 5) + ',' + (d.y + 12) + ')'; })
 				.style('opacity', 0)
 				.text(function(d) { return d.name; });
-			var rects = svg.selectAll('.topSong-data').data(data).enter()
+			var rects = svg.selectAll('.top-data').data(data).enter()
 				.append('rect')
-				.attr('class', 'topSong-data')
+				.attr('class', 'top-data')
 				.attr('width', 0)
 				.attr('height', function(d) { return d.h; })
 				.attr('transform', function(d) { return 'translate(' + x0 + ',' + d.y + ')'; })
 				.style('fill', function(d) { return getColor(d, genres); });
-			var countText = svg.selectAll('.topSong-count').data(data).enter()
+			var countText = svg.selectAll('.top-count').data(data).enter()
 				.append('text')
-				.attr('class', 'topSong-count')
+				.attr('class', 'top-count')
 				.attr('transform', function(d) { return 'translate(' + (x0 + 5) + ',' + (d.y + 12) + ')'; })
 				.text(function(d) { return d.data[z]; });
 			
 			// Transitions
-			svg.selectAll('.topSong-data').transition().duration(delay).attr('transform', function(d) { return 'translate(' + x0 + ',' + d.y + ')'; })
+			svg.selectAll('.top-data').transition().duration(delay).attr('transform', function(d) { return 'translate(' + x0 + ',' + d.y + ')'; })
 				.attr('width', function(d) { return scale(d.data[z]); }).style('fill', function(d) { return getColor(d, genres); })
-			svg.selectAll('.topSong-name').transition().duration(delay).attr('transform', function(d) { return 'translate(' + (x0 - 5) + ',' + (d.y + 12) + ')'; })
+			svg.selectAll('.top-name').transition().duration(delay).attr('transform', function(d) { return 'translate(' + (x0 - 5) + ',' + (d.y + 12) + ')'; })
 				.style('opacity', 1).text(function(d) { return d.name; });
-			svg.selectAll('.topSong-count').transition().duration(delay).style('opacity', 1).attr('transform', function(d) { return 'translate(' + (scale(d.data[z]) + x0 + 5) + ',' + (d.y + 15) + ')'; }).text(function(d) { return d.data[z]; });
-		}, svg.selectAll('.topSong-data')[0].length == 0 ? 0 : delay);
+			svg.selectAll('.top-count').transition().duration(delay).style('opacity', 1).attr('transform', function(d) { return 'translate(' + (scale(d.data[z]) + x0 + 5) + ',' + (d.y + 15) + ')'; }).text(function(d) { return d.data[z]; });
+		}, svg.selectAll('.top-data')[0].length == 0 ? 0 : delay);
 	}
 	
 	// Filters down to top songs
 	function getData(selected) {
-		var filtered = full.filter(function(d) { return selected.length == 0 || selected.indexOf(d['Genre']) != -1; })
+		var filtered = full.slice(0).filter(function(d) { return selected.length == 0 || selected.indexOf(d['Genre']) != -1; });
 		var raw = filtered.sort(function(a, b) { return (b[z] ? b[z] : 0) - (a[z] ? a[z] : 0); }).slice(0, Math.min(filtered.length, 25));
 		var h = (height - margin.top - margin.bottom) / raw.length -  pad;
 		return raw.map(function(d, i) {
@@ -176,14 +152,119 @@ function createTop(metric) {
 }
 
 /**
+ * Creates a distribution graph.
+ * @param z - Metric
+ * @param id - Id of chart container
+ * @param bucket - Bucket size
+ * @param fn - Optional function to preprocess metric data
+ */
+function createDistribution(z, id, bucket, fn) {
+	var margin = {left: 55, right: 20, top: 50, bottom: 40};
+	// Remove old svg
+	d3.select(id).select('svg').remove();
+	
+	// Create SVG
+	var svg = d3.select(id).append('svg')
+		.attr('width', width).attr('height', height);
+	
+	var xAxisEl = svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(" + margin.left + "," + (height - margin.bottom) + ")");
+	xAxisEl.append("text")
+		.attr("x", width - margin.right - 40)
+		.attr("dy", margin.bottom - 5)
+		.style("text-anchor", "end")
+		.text(z);
+	
+	var yAxisEl = svg.append("g")
+	    .attr("class", "y axis")
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+	yAxisEl.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", 10 - margin.left)
+		.style("text-anchor", "end")
+		.text("Count");
+	
+	// Initialize legend data
+	createLegend(full.slice(0), 'Genre', svg, 'Top Genres', refresh);
+	
+	refresh([])
+	
+	// Refreshes data
+	function refresh(selected) {
+		// Initialize top song data
+		var data = getData(selected);
+		var colors = d3.scale.category10();
+		
+		// Create bars and count text
+		var keyMax = d3.max(data, function(d) { return d.key; });
+		var scaleX = d3.scale.linear().domain([0, keyMax - keyMax % bucket + bucket]).range([0, width - margin.left - margin.right]);
+		var scaleY = d3.scale.linear().domain([0, d3.max(data, function(d) { return d.val; })]).range([height - margin.bottom - margin.top, 0]);
+		
+		var xAxis = d3.svg.axis()
+		    .scale(scaleX)
+		    .orient("bottom");
+		
+		var yAxis = d3.svg.axis()
+		    .scale(scaleY)
+		    .orient("left");
+		
+		svg.selectAll('.dist-data').data(data).exit().remove();
+		var rects = svg.selectAll('.dist-data').data(data).enter()
+			.append('rect')
+			.attr('class', 'dist-data')
+			.attr('width', function(d) { return d.w; })
+			.attr('height', 0)
+			.attr('transform', function(d) { return 'translate(' + (margin.left + scaleX(d.key)) + ',' + (height - margin.bottom) + ')'; })
+			.style('fill', function(d, i) { return colors(i); });
+		
+		// Transitions
+		svg.selectAll('.dist-data').transition().duration(delay)
+			.attr('transform', function(d) { return 'translate(' + (margin.left + scaleX(d.key)) + ',' + (scaleY(d.val) + margin.top) + ')'; })
+			.attr('height', function(d) { return height - margin.bottom - margin.top - scaleY(d.val); })
+			.attr('width', function(d) { return d.w; });
+
+		xAxisEl.transition().duration(delay).call(xAxis);
+		yAxisEl.transition().duration(delay).call(yAxis);
+	}
+	
+	// Aggregates filtered play count distributions
+	function getData(selected) {
+		var filtered = full.filter(function(d) { return selected.length == 0 || selected.indexOf(d['Genre']) != -1; });
+		filtered.forEach(function(d) {
+			d['Temp'] = d[z];
+			if (fn) {
+				d['Temp'] = fn(d['Temp']);
+			}
+			d['Temp'] -= d['Temp'] % bucket;
+		});
+		var raw = aggregateMetric(filtered, 'Temp');
+		var max = d3.max(raw, function(d) { return parseInt(d.name); });
+		var w = (width - margin.left - margin.right) / max;
+		return raw.map(function(d, i) {
+			return {w: w * bucket, val: d.val, key: (parseInt(d.name) - bucket)};
+		});
+	}
+	
+	// Get bar color
+	function getColor(d, genres) {
+		var c = $.grep(genres, function(g) { return g.name == d.data['Genre']; });
+		return c.length != 0 ? c[0].color : 'black';
+	}
+}
+
+/**
  * Creates legend items at attaches an on click function.
- * @param data - Array of objects containing name and val, val used to sort
+ * @param full - Full data set
+ * @param metric - Metric of full data set to be aggregated and turned into legend items
  * @param svg - Current SVG
  * @param label - Legend title
- * @param fn - Function to be executed on click, inputs are clicked item and this
+ * @param fn - Function to be executed on click, input is an array of the currently selected legend items
  * @returns Updated data array
  */
-function createLegend(data, svg, label, fn) {
+function createLegend(full, metric, svg, label, fn) {
+	var data = aggregateMetric(full, metric);
 	var colors = d3.scale.category10();
 	// Find genre text sizes
 	var total = svg.attr('width');
@@ -196,7 +277,19 @@ function createLegend(data, svg, label, fn) {
 	
 	// Create genre text so size can be measured
 	var legends = svg.selectAll('legend').data(data).enter().append('g').attr('class', 'legend').on('click', function(d) {
-		fn(d, d3.select(this));
+		var me = d3.select(this);
+		var classes = me.attr('class').split(' ');
+		if (classes.length == 1) {
+			me.select('rect').style('fill', d.color);
+			me.select('text').style('fill', 'white');
+			me.attr('class', classes[0] + ' selected');
+		} else {
+			me.select('rect').style('fill', 'none');
+			me.select('text').style('fill', '#555555');
+			me.attr('class', classes[0]);
+		}
+		var selected = svg.selectAll('.selected')[0].map(function(d) { return d3.select(d).select('text').text(); });
+		fn(selected);
 	});
 	// Create genre rectangles and text
 	legends.data(data).append('rect')
@@ -209,6 +302,47 @@ function createLegend(data, svg, label, fn) {
 		.attr('transform', function(d) { return 'translate(' + (d.x + d.w / 2) + ',30)'; })
 		.text(function(d) { return d.name; });
 	return data;
+}
+
+/**
+ * Aggregates a metric
+ * @param all - Array of data to be aggregated
+ * @param metric - Metric to be counted
+ * @returns {Array} Array of objects with name of metric value and count
+ */
+function aggregateMetric(full, metric) {
+	var data = [];
+	$.each(full.reduce(function(all, cur) {
+		if (all[cur[metric]]) {
+			all[cur[metric]]++;
+		} else if(cur[metric]) {
+			all[cur[metric]] = 1;
+		};
+		return all;
+	}, {}), function(k, v) {
+		data.push({name: k, val: v});
+	});
+	return data;
+}
+
+/**
+ * Highlights selected filter
+ * @param d
+ * @param me
+ */
+function legendFilter(d, me) {
+	var classes = me.attr('class').split(' ');
+	if (classes.length == 1) {
+		me.select('rect').style('fill', d.color);
+		me.select('text').style('fill', 'white');
+		me.attr('class', classes[0] + ' selected');
+	} else {
+		me.select('rect').style('fill', 'none');
+		me.select('text').style('fill', '#555555');
+		me.attr('class', classes[0]);
+	}
+	var selected = svg.selectAll('.selected')[0].map(function(d) { return d3.select(d).select('text').text(); });
+	refresh(selected);
 }
 
 // Finds the true size of text when rendered
